5810dbd92c6e7dbda2e3e01ce551692e0e1b8187
Filip Mularczyk <filip.mularczyk@pl.abb.com>
2021-12-11 21:22:30 +0100

    add day10 solution

--- AdventOfCode2021/Day10/__init__.py
+++ AdventOfCode2021/Day10/__init__.py
@@ -1,6 +1,6 @@
 import time
 from collections import deque
-from typing import List, Optional
+from typing import List, Optional, Sequence
 
 closing_mapping = {
     "(": ")",
@@ -9,11 +9,18 @@ closing_mapping = {
     "<": ">",
 }
 
-score_mapping = {
+p1_score_mapping = {
     ")": 3,
     "]": 57,
     "}": 1197,
     ">": 25137,
+}
+
+p2_score_mapping = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
 }
 
 
@@ -38,22 +45,28 @@ def find_closing_brackets(line: str) -> Optional[List[str]]:
         expected_closing_char = closing_mapping[d.popleft()]
         if expected_closing_char != char:
             return None
-    return list(closing_mapping[element] for element in d)
+    return [closing_mapping[element] for element in d]
 
 
-def count_broken_score(results: List[Optional[str]]) -> int:
+def count_broken_score(results) -> int:
     score = 0
     for result in results:
-        score += score_mapping.get(result, 0)
+        score += p1_score_mapping.get(result, 0)
     return score
 
 
-def count_closing_score(brackets: List[str]) -> int:
-    score = 0
+def count_closing_score(results) -> int:
+    scores = []
     for result in results:
-        score += score_mapping.get(result, 0)
-    return score
-
+        if not result:
+            continue
+        score = 0
+        for bracket in result:
+            score *= 5
+            score += p2_score_mapping.get(bracket, 0)
+        scores.append(score)
+    middle_score = sorted(scores)[len(scores) // 2]
+    return middle_score
 
 
 def load_case(path: str) -> List[str]:
@@ -66,20 +79,16 @@ if __name__ == '__main__':
     test_data = load_case("test.txt")
     test_result_p1 = count_broken_score(find_broken_line(line) for line in test_data)
     assert test_result_p1 == 26397
-    # test_sizes = sorted([get_basin_size(test_data, point) for point in np.argwhere(test_min_values)], reverse=True)
-    for line in test_data:
-        print(find_closing_brackets(line))
-    # test_result_p2 = test_sizes[0] * test_sizes[1] * test_sizes[2]
-    # assert test_result_p2 == 288957
+    test_result_p2 = count_closing_score(find_closing_brackets(line) for line in test_data)
+    assert test_result_p2 == 288957
 
     # Part 1
     data = load_case("data.txt")
     t = time.perf_counter()
     result_p1 = count_broken_score(find_broken_line(line) for line in data)
-    print(f"Part1: {result_p1:>8} (t: {1000 * (time.perf_counter() - t):.3f} ms)")
+    print(f"Part1: {result_p1:>10} (t: {1000 * (time.perf_counter() - t):.3f} ms)")
 
     # Part 2
     t = time.perf_counter()
-    # basin_sizes = sorted([get_basin_size(data, location) for location in np.argwhere(min_values)], reverse=True)
-    # result_p2 = basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
-    # print(f"Part2: {result_p2:>8} (t: {1000 * (time.perf_counter() - t):.3f} ms)")
+    result_p2 = values = count_closing_score(find_closing_brackets(line) for line in data)
+    print(f"Part2: {result_p2:>10} (t: {1000 * (time.perf_counter() - t):.3f} ms)")
